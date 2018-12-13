148e37158833f108ea06d4cb5e84d0aa59b69ebf
Gregory Norton <gregory.norton@me.com>
2018-12-12 19:15:48 -0500

    Added converting input string to upper and locating instruction

--- src/main.c
+++ src/main.c
@@ -11,27 +11,43 @@ char buffer[LINEBUFFERSIZE];
 
 void configure(int argc, char *argv[]);
 static Architecture *str_to_arch(const char arch_name[]);
+static char *str_to_upper(char str[]);
 Instruction *find_instruction(const char *mnem);
 
+char *out_fname;
 FILE *in;
 FILE *out;
 Architecture *arch;
+size_t address;
 
 int main(int argc, char **argv) {
+    out_fname = strdup("a.out");
     in = stdin;
-    out = stdout;
+    //out = stdout;
     arch = architectures;
-    
+    address = 0;
+
     configure(argc, argv);
-    
-    /*printf("%s\n", instructions[0].mne);
+
+    //out = fopen(out_fname, "w+");
+    free(out_fname);
     
     while (fgets(buffer, LINEBUFFERSIZE, in) != NULL) {
-        fprintf(out, "%s", buffer);
-    }*/
+        str_to_upper(buffer);
+        //Instruction *i = find_instruction(str_to_upper(buffer));
+        fprintf(out, "buffer after conversion contains %s\n", buffer);
+        
+        
+        Instruction *i = find_instruction(buffer);
+        if (i != NULL) {
+            fprintf(out, "%s, %X, %X\n", i->mne, i->base_opcode, i->regs);
+        }
+    }
     
-    printf("%s\n", find_instruction("ADC")->mne);
-    printf("%s\n", arch->name);
+    //fclose(out);
+
+    /*printf("%s\n", find_instruction("ADC")->mne);
+    printf("%s\n", arch->name);*/
     
 	return 0;
 }
@@ -43,11 +59,12 @@ void configure(int argc, char *argv[]) {
             case 'm':
                 arch = str_to_arch(optarg);
                 if (arch == NULL) {
-                    die("Unsupported architecture");
+                    die("Unsupported architecture: %s\n", optarg);
                 }
                 break;
             case 'o':
-                out = fopen(optarg, "w+");
+                free(out_fname);
+                out_fname = strdup(optarg);
                 break;
             case 'f':
                 break;
@@ -66,8 +83,22 @@ static Architecture *str_to_arch(const char arch_name[]) {
     return NULL;
 }
 
+static char *str_to_upper(char *str) {
+    char *c;
+    for (c = str; *c != '\0'; c++) {
+        if (*c >= 'a' && *c <= 'z') {
+            *c += 'A' - 'a';
+        }
+        else if (*c == '\n') {
+            *c = '\0';
+        }
+    }
+    return str;
+}
+
 Instruction *find_instruction(const char *mnem) {
     for (Instruction *i = instructions; i->mne[0] != '\0'; i++) {
+        printf("%s\t%s\n", mnem, i->mne);
         if (streq(mnem, i->mne)) {
             return i;
         }
