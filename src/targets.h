3364d78939e2ff0e68770422fae4e05095cc8cf2
Gregory Norton <Gregory.Norton@me.com>
2019-07-28 18:57:25 -0400

    Added setup code for a 6502 target

--- src/targets.h
+++ src/targets.h
@@ -3,6 +3,7 @@
 
 #include <targets/6x09/arch.h>
 #include <targets/z80/arch.h>
+//#include <targets/6502/arch.h>
 
 #define str(s) #s
 #define INIT_TARGET(target) target##_init() 
@@ -10,8 +11,9 @@
     INIT_TARGET(MC6809); \
     INIT_TARGET(HD6309); \
     INIT_TARGET(Z80); \
+    //INIT_TARGET(MOS6502); \
 }
-#define TARGETS &ARCH_MC6809, &ARCH_HD6309, &ARCH_Z80, NULL
+#define TARGETS &ARCH_MC6809, &ARCH_HD6309, &ARCH_Z80, /*&ARCH_MOS6502,*/ NULL
 
 //extern Architecture **architectures[];
 
