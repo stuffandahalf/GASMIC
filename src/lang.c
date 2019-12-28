1d286ac3ffa91a8d2d07ad27ed9ecb537ff1c5a6
Gregory Norton <gregory.norton@me.com>
2019-12-28 08:44:50 -0500

    Added missing return statement to printef

--- src/lang.c
+++ src/lang.c
@@ -4,10 +4,13 @@
 
 int printef(const char *msg, ...)
 {
+    int c;
     va_list args;
     va_start(args, msg);
 
-    vfprintf(stderr, msg, args);
+    c = vfprintf(stderr, msg, args);
 
     va_end(args);
+
+    return c;
 }
