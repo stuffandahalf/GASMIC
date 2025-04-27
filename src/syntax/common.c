0ca9fdb826825d36b8c9505cc401ec4ff944052e
Gregory Norton <gregory.norton@me.com>
2025-04-27 01:02:34 -0400

    Added parsing for expressions in parenthesis

--- src/syntax/common.c
+++ src/syntax/common.c
@@ -3,7 +3,41 @@
 int
 evaluate_expr(struct line_arg *la, const char *buffer)
 {
-	return 0;
+	int i = 0, consumed;
+
+	i += countspaces(buffer);
+	if ((buffer[i] == '(')) {
+		if (!(consumed = evaluate_paren(la, &buffer[i]))) {
+			return 0;
+		}
+		i += consumed;
+	}
+	i += countspaces(buffer);
+
+	return i;
+}
+
+int
+evaluate_paren(struct line_arg *la, const char *buffer)
+{
+	int i = 0, consumed;
+
+	/* \s*\(\s*<expr>\s*\) */
+	i += countspaces(buffer);
+	if (buffer[i++] != '(') {
+		return 0;
+	}
+	i += countspaces(buffer);
+	if (!(consumed = evaluate_expr(la, &buffer[i]))) {
+		return 0;
+	}
+	i += consumed;
+	i += countspaces(buffer);
+	if (buffer[i++] != ')') {
+		return 0;
+	}
+
+	return i;
 }
 
 int
